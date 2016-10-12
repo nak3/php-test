d527e4d7893aeb5c7753579063db22f0d804cbf6
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2016-10-12 11:15:21 +0900

    Output environmental variables

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 <h1>PHP is working</h1>
 <p>
 <?php
-	phpinfo();
+    while(list($key, $value) = each($_SERVER)) {
+        echo "<tr><td>$key</td><td>", $value, "</td></tr>\n";
+    }
 ?>
 </p>
 </body>
